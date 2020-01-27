0068fe50f019f0cf0e355c626e7b7d5c19ef9990
yurii.k <yurii.k@sbtech.com>
2020-01-27 18:16:57 +0200

    fixes

---
+++ AutomationTryOut/BasePage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using OpenQA.Selenium;
+
+namespace AutomationTryOut
+{
+    internal class BasePage
+    {
+
+        protected IWebDriver Driver { get; set;}
+        public BasePage(IWebDriver driver)
+        {
+            Driver = driver;
+        }
+    }
+}
